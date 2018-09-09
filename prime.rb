94c2f5e753f32324fbafe6df4726f67de4ef4cea
Andrew Bernier <a6bernier@gmail.com>
2018-09-09 17:12:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(integer)
-  if integer < 0 || integer == 0 || integer == 1 
+  if integer < 0 || integer == 0 || integer == 1
     return false
   else
     (2..integer-1).to_a.all? do |possible|
-      integer % possible != 0 
+      integer % possible != 0
     end
   end
 end
